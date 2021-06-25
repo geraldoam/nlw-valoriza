daed89c2367c52468330df42540fd167acabff7b
geraldoam <geraldoahnert@gmail.com>
2021-06-25 19:21:33 -0300

    feat: list user receive and send compliments

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import { ensureAdmin } from './middlewares/ensureAdmin';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateComplimentController } from './controllers/CreateComplimentController';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
+import { ListUserSendComplimentsController } from './controllers/ListUserSendComplimentsController';
+import { ListUserReceiveComplimentsController } from './controllers/ListUserReceiveComplimentsController';
 
 const router = Router();
 
@@ -12,6 +14,10 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserService = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController =
+    new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController =
+    new ListUserReceiveComplimentsController();
 
 router.post('/users', createUserController.handle);
 router.post(
@@ -25,6 +31,17 @@ router.post(
     '/compliment',
     ensureAuthenticated,
     createComplimentController.handle
+);
+
+router.get(
+    '/users/compliments/send',
+    ensureAuthenticated,
+    listUserSendComplimentsController.handle
+);
+router.get(
+    '/users/compliments/receive',
+    ensureAuthenticated,
+    listUserReceiveComplimentsController.handle
 );
 
 export { router };
